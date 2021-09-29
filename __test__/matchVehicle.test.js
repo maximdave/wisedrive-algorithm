1c30d57e413199befe7ce78b53688c162366e154
David Enoragbon <david.enoragbon@decagon.dev>
2021-09-29 21:36:03 +0100

    added test files

--- __test__/matchVehicle.test.js
+++ __test__/matchVehicle.test.js
@@ -1,12 +1,17 @@
-const models = require("../output/model.json");
 const findCarByModel = require("../index");
-const types = require("../output/types.json");
 
 describe("Match Vehicle", () => {
-  it("should return a matching Vehicle", async () => {
-    // expect(1+1).toBe(2)
-   const result =  findCarByModel("520D XDRIVE", "2017");
-   const expected = [ '106275', '106459' ]
-   expect(result).arrayContaining(expected)
+  const expected = ["106275", "106459"];
+  it("should return a matching Vehicle", () => {
+    expect(findCarByModel("520D XDRIVE", "2017")).toEqual(
+      expect.arrayContaining(expected)
+    );
+  });
+});
+
+describe("No Vehicle found", () => {
+  const expected = 'Car type not found';
+  it("should not return a matching Vehicle", () => {
+    expect(findCarByModel("520D", "9017")).toEqual(expected);
   });
 });
